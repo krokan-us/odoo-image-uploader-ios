765f98d3759a8db6c6fad5e5e9391aeef698b752
Mustafa Asım Altınışık <mustafa@altinisik.net>
2023-06-04 18:25:53 +0300

    configure function is added.

--- Odoo-iOS/Cells/ImageCollectionViewCell.swift
+++ Odoo-iOS/Cells/ImageCollectionViewCell.swift
@@ -11,7 +11,30 @@ class ImageCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var isSelectedButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        isSelectedButton.setTitle("", for: .normal)
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.black.cgColor
+        layer.cornerRadius = 10
+    }
+    
+    func configure(with image: ProductImage) {
+        if image.isPublished {
+            isSelectedButton.setImage(UIImage(systemName: "eye"), for: .normal)
+        } else {
+            isSelectedButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+        }
+        
+        if let imageData = Data(base64Encoded: image.imageData) {
+            print("Successfully loaded image data for image ID: \(image.id)")
+            photoImageView.image = UIImage(data: imageData)
+        } else {
+            //Fail report will be sent here.
+            print("Failed to load image data for image ID: \(image.id)")
+            photoImageView.image = UIImage(named: "corruptedImage")
+            isSelectedButton.isHidden = true
+        }
     }
 }
