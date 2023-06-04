a7f2458352cfb72b5a87bb739feeb44cc0855c8e
Mustafa Asım Altınışık <mustafa@altinisik.net>
2023-06-04 16:04:28 +0300

    ImageCollectionViewCell is added.

---
+++ Odoo-iOS/Cells/ImageCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCollectionViewCell.swift
+//  Odoo-iOS
+//
+//  Created by Asım Altınışık on 4.06.2023.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var isSelectedButton: UIButton!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
