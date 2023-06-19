56ff8e8f74cd47764bdc5ca274980d9a7c821913
Mustafa Asım Altınışık <mustafa@altinisik.net>
2023-06-19 14:38:42 +0300

    URL and Database fields are moved to another view controller.

--- Odoo-iOS/ViewControllers/LoginViewController.swift
+++ Odoo-iOS/ViewControllers/LoginViewController.swift
@@ -10,21 +10,14 @@ import DropDown
 
 class LoginViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate {
     let dropDown = DropDown()
+    @IBOutlet weak var networkSettingsButton: UIButton!
     @IBOutlet weak var changeUserView: UIView!
     @IBOutlet weak var changeUserButton: UIButton!
     @IBOutlet weak var profileImageView: UIImageView!
     
     @IBOutlet weak var welcomeLabel: UILabel!
-
-    @IBOutlet weak var URLView: UIView!
-    @IBOutlet weak var URLLabel: UILabel!
-    @IBOutlet weak var URLTextView: UITextView!
-    @IBOutlet weak var URLClearButton: UIButton!
     
-    @IBOutlet weak var databaseView: UIView!
-    @IBOutlet weak var databaseLabel: UILabel!
-    @IBOutlet weak var databaseTextView: UITextView!
-    @IBOutlet weak var databaseClearButton: UIButton!
+    @IBOutlet weak var textViewsStack: UIStackView!
     
     @IBOutlet weak var usernameView: UIView!
     @IBOutlet weak var usernameLabel: UILabel!
@@ -50,8 +43,6 @@ class LoginViewController: UIViewController, UITextViewDelegate, UITextFieldDele
         configureChangeUserButton()
         updateLoginButtonState()
         
-        URLTextView.delegate = self
-        databaseTextView.delegate = self
         usernameTextView.delegate = self
         passwordTextField.delegate = self
     }
@@ -66,14 +57,6 @@ class LoginViewController: UIViewController, UITextViewDelegate, UITextFieldDele
     }
     
     private func setViewBackgrounds(){
-        URLView.layer.borderWidth = 1
-        URLView.layer.borderColor = UIColor.label.cgColor
-        URLView.layer.cornerRadius = 10
-        
-        databaseView.layer.borderWidth = 1
-        databaseView.layer.borderColor = UIColor.label.cgColor
-        databaseView.layer.cornerRadius = 10
-        
         usernameView.layer.borderWidth = 1
         usernameView.layer.borderColor = UIColor.label.cgColor
         usernameView.layer.cornerRadius = 10
@@ -95,8 +78,6 @@ class LoginViewController: UIViewController, UITextViewDelegate, UITextFieldDele
     
     func setScreenForLastlyLoggedUser() {
         if let lastLoggedUser = LoggedUsersManager.shared.getLastlyLoggedUser() {
-            URLTextView.text = lastLoggedUser.URL.absoluteString
-            databaseTextView.text = lastLoggedUser.database
             usernameTextView.text = lastLoggedUser.username
             if let imageData = Data(base64Encoded: lastLoggedUser.profileImageData),
                let image = UIImage(data: imageData) {
@@ -108,29 +89,29 @@ class LoginViewController: UIViewController, UITextViewDelegate, UITextFieldDele
             print("No logged-in users")
         }
     }
-
+    
     private func configureChangeUserDropdownMenu() {
         let loggedUsers = LoggedUsersManager.shared.getLoggedUsers()
         let dropdownItems = loggedUsers.map { $0.userName.split(separator: " ").first?.description ?? "" }
-
+        
         dropDown.anchorView = changeUserView
         dropDown.dataSource = dropdownItems
-
+        
         dropDown.cellNib = UINib(nibName: "ChangeProfileCell", bundle: nil)
-
+        
         dropDown.customCellConfiguration = { (index: Index, item: String, cell: DropDownCell) -> Void in
             guard let cell = cell as? ChangeProfileCell else { return }
-
+            
             // Setup your custom UI components
             let loggedUser = loggedUsers[index]
             cell.profileImage.image = UIImage(data: Data(base64Encoded: loggedUser.profileImageData) ?? Data())
         }
-
+        
         dropDown.selectionAction = { [weak self] (index: Int, item: String) in
             // Update the login page with the selected user's information
             let selectedUser = loggedUsers[index]
-            self?.URLTextView.text = selectedUser.URL.absoluteString
-            self?.databaseTextView.text = selectedUser.database
+            UserDefaults.standard.set(selectedUser.URL.absoluteString, forKey: "URLTextViewValue")
+            UserDefaults.standard.set(selectedUser.database, forKey: "DatabaseTextViewValue")
             self?.usernameTextView.text = selectedUser.username
             if let imageData = Data(base64Encoded: selectedUser.profileImageData),
                let image = UIImage(data: imageData) {
@@ -143,31 +124,21 @@ class LoginViewController: UIViewController, UITextViewDelegate, UITextFieldDele
             UINotificationFeedbackGenerator().notificationOccurred(.success)
         }
     }
-
+    
     private func configureLabels(){
         welcomeLabel.text = NSLocalizedString("welcome", comment: "")
-        databaseLabel.text = NSLocalizedString("database", comment: "")
         usernameLabel.text = NSLocalizedString("username", comment: "")
         passwordLabel.text = NSLocalizedString("password", comment: "")
     }
-
+    
     private func configureTextViews() {
-        URLTextView.isScrollEnabled = false
-        URLTextView.textContainer.lineBreakMode = .byTruncatingTail
-        URLTextView.textContainer.maximumNumberOfLines = 1
-        
-        databaseTextView.isScrollEnabled = false
-        databaseTextView.textContainer.lineBreakMode = .byTruncatingTail
-        databaseTextView.textContainer.maximumNumberOfLines = 1
-        
         usernameTextView.isScrollEnabled = false
         usernameTextView.textContainer.lineBreakMode = .byTruncatingTail
         usernameTextView.textContainer.maximumNumberOfLines = 1
     }
     
     private func configureButtons(){
-        URLClearButton.setTitle("", for: .normal)
-        databaseClearButton.setTitle("", for: .normal)
+        networkSettingsButton.setTitle("", for: .normal)
         usernameClearButton.setTitle("", for: .normal)
         passwordClearButton.setTitle("", for: .normal)
         
@@ -184,40 +155,49 @@ class LoginViewController: UIViewController, UITextViewDelegate, UITextFieldDele
     }
     
     @IBAction func loginButtonTapped(_ sender: Any) {
-        if let urlText = URLTextView.text, let url = URL(string: urlText) {
-            NetworkManager.shared.sendLoginRequest(baseURL: url, databaseName: databaseTextView.text, username: usernameTextView.text, password: passwordTextField.text ?? "") {
-                userID in
-                if let userID = userID {
-                    print("Login successful! User ID: \(userID)")
-                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                    if let tabBarVC = storyboard.instantiateViewController(withIdentifier: "tabBar") as? TabBarViewController {
-                        tabBarVC.modalPresentationStyle = .fullScreen
-                        DispatchQueue.main.async {
-                            self.present(tabBarVC, animated: false)
-
-                            let userID = userID
-                            NetworkManager.shared.getLoggedUserDetails(userID: userID) { userDetails in
-                                if let userDetails = userDetails {
-                                    LoggedUsersManager.shared.addLoggedUser(user: LoggedUser(id: userDetails.userID, username: self.usernameTextView.text, URL: url, database: self.databaseTextView.text, userName: userDetails.userName, profileImageData: userDetails.imageData, lastLoginDate: Date()))
-                                    print(LoggedUsersManager.shared.getLoggedUsers())
-                                } else {
-                                    print("Failed to fetch user details")
-                                }
+        guard let database = UserDefaults.standard.string(forKey: "DatabaseTextViewValue"),
+              let urlString = UserDefaults.standard.string(forKey: "URLTextViewValue"),
+              let url = URL(string: urlString) else {
+            displayEmptyValuesAlert()
+            return
+        }
+        
+        NetworkManager.shared.sendLoginRequest(baseURL: url, databaseName: database, username: usernameTextView.text, password: passwordTextField.text ?? "") { userID in
+            if let userID = userID {
+                print("Login successful! User ID: \(userID)")
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                if let tabBarVC = storyboard.instantiateViewController(withIdentifier: "tabBar") as? TabBarViewController {
+                    tabBarVC.modalPresentationStyle = .fullScreen
+                    DispatchQueue.main.async {
+                        self.present(tabBarVC, animated: false)
+                        
+                        let userID = userID
+                        NetworkManager.shared.getLoggedUserDetails(userID: userID) { userDetails in
+                            if let userDetails = userDetails {
+                                LoggedUsersManager.shared.addLoggedUser(user: LoggedUser(id: userDetails.userID, username: self.usernameTextView.text, URL: url, database: database, userName: userDetails.userName, profileImageData: userDetails.imageData, lastLoginDate: Date()))
+                                print(LoggedUsersManager.shared.getLoggedUsers())
+                            } else {
+                                print("Failed to fetch user details")
                             }
                         }
                     }
-                } else {
-                    UINotificationFeedbackGenerator().notificationOccurred(.error)
-                    let alert = UIAlertController(title: NSLocalizedString("loginFailed", comment: ""), message: NSLocalizedString("tryAgain", comment: ""), preferredStyle: .alert)
-                    let okAction = UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: nil)
-                    alert.addAction(okAction)
-                    self.present(alert, animated: true, completion: nil)
-                    print("Login failed.")
                 }
+            } else {
+                UINotificationFeedbackGenerator().notificationOccurred(.error)
+                let alert = UIAlertController(title: NSLocalizedString("loginFailed", comment: ""), message: NSLocalizedString("tryAgain", comment: ""), preferredStyle: .alert)
+                let okAction = UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: nil)
+                alert.addAction(okAction)
+                self.present(alert, animated: true, completion: nil)
+                print("Login failed.")
             }
-        } else {
-            print("Invalid URL")
         }
+    }
+    
+    func displayEmptyValuesAlert() {
+        let alert = UIAlertController(title: NSLocalizedString("missingValues", comment: ""), message: NSLocalizedString("pleaseFillTheURLAndDatabaseFields", comment: ""), preferredStyle: .alert)
+        let okAction = UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
     }
     
     @objc private func handleTap() {
@@ -231,19 +211,19 @@ class LoginViewController: UIViewController, UITextViewDelegate, UITextFieldDele
     func textViewDidChange(_ textView: UITextView) {
         updateLoginButtonState()
     }
+    
+    @IBAction func networkSettingsButtonTapped(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let networkSettingsScreen = storyboard.instantiateViewController(withIdentifier: "networkSettings") as? NetworkSettingsViewController {
+            networkSettingsScreen.modalPresentationStyle = .popover
+            DispatchQueue.main.async {
+                self.present(networkSettingsScreen, animated: true)
+            }
+        }
+    }
 }
 
 extension LoginViewController {
-    @IBAction func URLClearButtonTapped(_ sender: Any) {
-        URLTextView.text = ""
-        updateLoginButtonState()
-    }
-    
-    @IBAction func databaseClearButtonTapped(_ sender: Any) {
-        databaseTextView.text = ""
-        updateLoginButtonState()
-    }
-    
     @IBAction func usernameClearButtonTapped(_ sender: Any) {
         usernameTextView.text = ""
         updateLoginButtonState()
@@ -255,11 +235,9 @@ extension LoginViewController {
     }
     
     private func updateLoginButtonState() {
-        let isURLEmpty = URLTextView.text.isEmpty
-        let isDatabaseEmpty = databaseTextView.text.isEmpty
         let isUsernameEmpty = usernameTextView.text.isEmpty
         let isPasswordEmpty = passwordTextField.text?.isEmpty ?? true
         
-        loginButton.isEnabled = !(isURLEmpty || isDatabaseEmpty || isUsernameEmpty || isPasswordEmpty)
+        loginButton.isEnabled = !(isUsernameEmpty || isPasswordEmpty)
     }
 }
